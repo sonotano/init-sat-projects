33886ac5840501592cb1a649d4a650ff450fec20
sonotano <alfredo_becerril@hotmail.com>
2016-09-26 17:20:46 -0500

    Add files via upload
    tablas de isr para persona fisica 2016, insert mysql

---
+++ crear tabla tablaisr.sql
@@ -0,0 +1,8 @@
+create table tablaisr
+(tablaisrid int,
+limiteinferior numeric(12,2),
+limitesuperior numeric(12,2),
+cuotafija numeric(12,2),
+porcentajeexcedente numeric(12,2),
+mes int,
+anio int)
---
+++ insert tabla isr 2016.sql
@@ -0,0 +1,153 @@
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,496.07,0,0.0192,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(496.08,4210.41,9.52,0.064,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(4210.42,7399.42,247.23,0.1088,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(7399.43,8601.5,594.24,0.16,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(8601.51,10298.35,786.55,0.1792,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(10298.36,20770.29,1090.62,0.2136,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(20770.3,32736.83,3327.42,0.2352,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(32736.84,62500,6141.95,0.3,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(62500.01,83333.33,15070.9,0.32,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(83333.34,250000,21737.57,0.34,1,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(250000.01,10000000,78404.23,0.35,1,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,992.14,0,0.0192,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(992.15,8420.82,19.04,0.064,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(8420.83,14798.84,494.46,0.1088,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(14798.85,17203,1188.48,0.16,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(17203.01,20596.7,1573.1,0.1792,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(20596.71,41540.58,2181.24,0.2136,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(41540.59,65473.66,6654.84,0.2352,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(65473.67,125000,12283.9,0.3,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(125000.01,166666.66,30141.8,0.32,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(166666.67,500000,43475.14,0.34,2,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(500000.01,10000000,156808.46,0.35,2,2016);
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,1488.21,0,0.0192,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1488.22,12631.23,28.56,0.064,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(12631.24,22198.26,741.69,0.1088,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(22198.27,25804.5,1782.72,0.16,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(25804.51,30895.05,2359.65,0.1792,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(30895.06,62310.87,3271.86,0.2136,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(62310.88,98210.49,9982.26,0.2352,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(98210.5,187500,18425.85,0.3,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(187500.01,249999.99,45212.7,0.32,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(250000,750000,65212.71,0.34,3,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(750000.01,10000000,235212.69,0.35,3,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,1984.28,0,0.0192,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1984.29,16841.64,38.08,0.064,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(16841.65,29597.68,988.92,0.1088,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(29597.69,34406,2376.96,0.16,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(34406.01,41193.4,3146.2,0.1792,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(41193.41,83081.16,4362.48,0.2136,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(83081.17,130947.32,13309.68,0.2352,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(130947.33,250000,24567.8,0.3,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(250000.01,333333.32,60283.6,0.32,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(333333.33,1000000,86950.28,0.34,4,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1000000.01,10000000,313616.92,0.35,4,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,2480.35,0,0.0192,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2480.36,21052.05,47.6,0.064,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(21052.06,36997.1,1236.15,0.1088,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(36997.11,43007.5,2971.2,0.16,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(43007.51,51491.75,3932.75,0.1792,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(51491.76,103851.45,5453.1,0.2136,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(103851.46,163684.15,16637.1,0.2352,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(163684.16,312500,30709.75,0.3,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(312500.01,416666.65,75354.5,0.32,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(416666.66,1250000,108687.85,0.34,5,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1250000.01,10000000,392021.15,0.35,5,2016);
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,2976.42,0,0.0192,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2976.43,25262.46,57.12,0.064,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(25262.47,44396.52,1483.38,0.1088,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(44396.53,51609,3565.44,0.16,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(51609.01,61790.1,4719.3,0.1792,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(61790.11,124621.74,6543.72,0.2136,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(124621.75,196420.98,19964.52,0.2352,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(196420.99,375000,36851.7,0.3,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(375000.01,499999.98,90425.4,0.32,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(499999.99,1500000,130425.42,0.34,6,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1500000.01,10000000,470425.38,0.35,6,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,3472.49,0,0.0192,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(3472.5,29472.87,66.64,0.064,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(29472.88,51795.94,1730.61,0.1088,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(51795.95,60210.5,4159.68,0.16,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(60210.51,72088.45,5505.85,0.1792,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(72088.46,145392.03,7634.34,0.2136,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(145392.04,229157.81,23291.94,0.2352,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(229157.82,437500,42993.65,0.3,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(437500.01,583333.31,105496.3,0.32,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(583333.32,1750000,152162.99,0.34,7,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1750000.01,10000000,548829.61,0.35,7,2016);
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,3968.56,0,0.0192,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(3968.57,33683.28,76.16,0.064,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(33683.29,59195.36,1977.84,0.1088,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(59195.37,68812,4753.92,0.16,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(68812.01,82386.8,6292.4,0.1792,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(82386.81,166162.32,8724.96,0.2136,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(166162.33,261894.64,26619.36,0.2352,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(261894.65,500000,49135.6,0.3,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(500000.01,666666.64,120567.2,0.32,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(666666.65,2000000,173900.56,0.34,8,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2000000.01,10000000,627233.84,0.35,8,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,4464.63,0,0.0192,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(4464.64,37893.69,85.68,0.064,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(37893.7,66594.78,2225.07,0.1088,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(66594.79,77413.5,5348.16,0.16,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(77413.51,92685.15,7078.95,0.1792,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(92685.16,186932.61,9815.58,0.2136,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(186932.62,294631.47,29946.78,0.2352,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(294631.48,562500,55277.55,0.3,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(562500.01,749999.97,135638.1,0.32,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(749999.98,2250000,195638.13,0.34,9,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2250000.01,10000000,705638.07,0.35,9,2016);
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,4960.7,0,0.0192,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(4960.71,42104.1,95.2,0.064,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(42104.11,73994.2,2472.3,0.1088,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(73994.21,86015,5942.4,0.16,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(86015.01,102983.5,7865.5,0.1792,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(102983.51,207702.9,10906.2,0.2136,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(207702.91,327368.3,33274.2,0.2352,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(327368.31,625000,61419.5,0.3,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(625000.01,833333.3,150709,0.32,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(833333.31,2500000,217375.7,0.34,10,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2500000.01,10000000,784042.3,0.35,10,2016);
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,5456.77,0,0.0192,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(5456.78,46314.51,104.72,0.064,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(46314.52,81393.62,2719.53,0.1088,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(81393.63,94616.5,6536.64,0.16,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(94616.51,113281.85,8652.05,0.1792,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(113281.86,228473.19,11996.82,0.2136,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(228473.2,360105.13,36601.62,0.2352,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(360105.14,687500,67561.45,0.3,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(687500.01,916666.63,165779.9,0.32,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(916666.64,2750000,239113.27,0.34,11,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(2750000.01,10000000,862446.53,0.35,11,2016);
+
+
+
+
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(0.01,5952.84,0,0.0192,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(5952.85,50524.92,114.24,0.064,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(50524.93,88793.04,2966.76,0.1088,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(88793.05,103218,7130.88,0.16,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(103218.01,123580.2,9438.6,0.1792,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(123580.21,249243.48,13087.44,0.2136,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(249243.49,392841.96,39929.04,0.2352,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(392841.97,750000,73703.4,0.3,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(750000.01,1000000,180850.82,0.32,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(1000000.01,3000000,260850.81,0.34,12,2016);
+insert into tablaisr(limiteinferior, limitesuperior, cuotafija, porcentajeexcedente, mes, anio) values(3000000.01,10000000,940850.81,0.35,12,2016);
+
